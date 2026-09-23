ae0b6428edcf10b6849d834d577c5e5c8df15383
enderGimbi <89043525046@mail.ru>
2026-09-23 14:10:16 +0300

    решено

--- src/main/java/org/softwaretechnologies/employee/Tester.java
+++ src/main/java/org/softwaretechnologies/employee/Tester.java
@@ -11,6 +11,6 @@ public class Tester extends Employee{
 
     @Override
     public int getMonthSalary(int month) {
-        return baseSalary * YearMonth.of(LocalDate.now().getYear(),month).getMonthValue();
+        return baseSalary * YearMonth.of(LocalDate.now().getYear(),month).lengthOfMonth();
     }
 }
